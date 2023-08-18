00ccb99a22eb5450f8957e0341c90d9a1f333d07
user1 <applecare@Apples-MacBook-Pro.local>
2023-08-18 17:45:28 +0530

    videos

--- components/Gallery.js
+++ components/Gallery.js
@@ -69,6 +69,32 @@ const Gallery = () => {
                         </div>
                     </div>
                 </div>
+              <div cl>    
+                <h2 className='block-title__title' style={{marginTop:50, marginBottom:50, textAlign:'center'}}>Testimonials</h2>
+                <div className="col-lg-4 col-md-6">
+                        <div className="gallery-one__single">
+                            {/* <img src="/assets/images/video1.mp4" alt="" width="370" height="370"/> */}
+                            <h3 className='block-title__title' style={{color:'grey', textAlign:'center', paddingTop:100}}>Video-1</h3>
+                            
+                            <video>
+                                <source src='/assets/images/video.mp4' className="gallery-one__popup img-popup"/>
+                            </video>
+                            
+                                <a href="assets/images/video1.mp4" className="gallery-one__popup img-popup"><i
+                                    className="kipso-icon-plus-symbol"></i></a>
+                        </div>
+                        <div className="gallery-one__single">
+                            {/* <img src="/assets/images/video1.mp4" alt="" width="370" height="370"/> */}
+                            <h3 className='block-title__title' style={{color:'grey', textAlign:'center', paddingTop:100}}>Video-2</h3>
+                            <video>
+                                <source src='/assets/images/video.mp4' className="gallery-one__popup img-popup"/>
+                            </video>
+                            
+                                <a href="assets/images/video2.mp4" className="gallery-one__popup img-popup"><i
+                                    className="kipso-icon-plus-symbol"></i></a>
+                        </div>
+                    </div>
+              </div> 
             </div>
         </section>
     );
